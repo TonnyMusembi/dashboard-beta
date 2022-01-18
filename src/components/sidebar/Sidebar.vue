4da47116354bde63463e1e9977cc469ec63a72ce
Tonny <musembitonnyj@gmail.com>
2022-01-18 21:38:46 +0300

    add

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -25,7 +25,7 @@ export default {
     <SidebarLink to="/dashboard" icon="fas fa-columns">Dashboard</SidebarLink>
     <SidebarLink to="/analytics" icon="fas fa-chart-bar">Analytics</SidebarLink>
     <SidebarLink to="/friends" icon="fas fa-users">Friends</SidebarLink>
-    <SidebarLink to="/image" icon="fas fa-image">Images</SidebarLink>
+    <SidebarLink to="/About" icon="fas fa-image">Images</SidebarLink>
 
     <span
       class="collapse-icon"
