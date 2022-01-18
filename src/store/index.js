6680bfa4e1f28e893eef072b80b2355d3f56e17f
Tonny <musembitonnyj@gmail.com>
2022-01-18 21:34:21 +0300

    add

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,29 @@
-import { createStore } from "vuex";
+/* eslint-disable prettier/prettier */
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: () =>
+            import ("../views/Dashboard.vue"),
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () =>
+            import ("../views/About.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
+
+export default router;
